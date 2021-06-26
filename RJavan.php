a9fbb1e769e82ff0405260846a4f1d1a980bde39
Alireza Ahmand <m2001ahmand@gmail.com>
2021-06-26 11:06:26 +0430

    get host bug fixed

--- RJavan.php
+++ RJavan.php
@@ -6,8 +6,8 @@ error_reporting(0);
 
 /**
  * @author Incognito Coder
- * @copyright 2020 ICDev
- * @version 1.0
+ * @copyright 2020-2021 ICDev
+ * @version 1.3
  */
 class Downloader
 {
@@ -42,10 +42,17 @@ class Downloader
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
+            CURLOPT_COOKIEFILE => 'cookies.txt',
+            CURLOPT_COOKIEJAR => 'cookies.txt',
             CURLOPT_TIMEOUT => 0,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+                'Accept: */*',
+                'Pragma: no-cache'
+            ),
         ));
 
         $response = curl_exec($curl);
