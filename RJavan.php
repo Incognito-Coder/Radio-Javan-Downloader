719341b29a1ad18f60c44999382e5b5ced46b54f
Alireza Ahmand <m2001ahmand@gmail.com>
2021-07-05 17:16:57 +0000

    added a new function for get file cover picture

--- RJavan.php
+++ RJavan.php
@@ -7,7 +7,7 @@ error_reporting(0);
 /**
  * @author Incognito Coder
  * @copyright 2020-2021 ICDev
- * @version 1.3
+ * @version 1.5
  */
 class Downloader
 {
@@ -54,16 +54,15 @@ class Downloader
                 'Pragma: no-cache'
             ),
         ));
-
         $response = curl_exec($curl);
         curl_close($curl);
         $reslut = json_decode($response);
         if ($type == "music") {
-            return json_encode(['status' => true, 'result' => $reslut->host . "/media/mp3/mp3-256/$parsed.mp3", 'title' => $parsed], 128) . PHP_EOL;
+            return json_encode(['status' => true, 'type' => $this->MediaType(), 'result' => $reslut->host . "/media/mp3/mp3-256/$parsed.mp3", 'title' => $parsed, 'photo' => $this->CoverArt($url)], 128) . PHP_EOL;
         } elseif ($type == "podcast") {
-            return json_encode(['status' => true, 'result' => $reslut->host . "/media/podcast/mp3-192/$parsed.mp3", 'title' => $parsed], 128) . PHP_EOL;
+            return json_encode(['status' => true, 'type' => $this->MediaType(), 'result' => $reslut->host . "/media/podcast/mp3-192/$parsed.mp3", 'title' => $parsed, 'photo' => $this->CoverArt($url)], 128) . PHP_EOL;
         } elseif ($type == "video") {
-            return json_encode(['status' => true, 'result' => $reslut->host . "/media/music_video/hd/$parsed.mp4", 'title' => $parsed], 128) . PHP_EOL;
+            return json_encode(['status' => true, 'type' => $this->MediaType(), 'result' => $reslut->host . "/media/music_video/hd/$parsed.mp4", 'title' => $parsed, 'photo' => $this->CoverArt($url)], 128) . PHP_EOL;
         } else {
             return json_encode(['status' => false, 'result' => 'incorrect data or another error occured.'], 128) . PHP_EOL;
         }
@@ -96,5 +95,32 @@ class Downloader
         } elseif (strpos($file, 'videos') !== false) {
             return 'video';
         }
+    }
+    function CoverArt($url)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_COOKIEFILE => 'cookies.txt',
+            CURLOPT_COOKIEJAR => 'cookies.txt',
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+                'Accept: */*',
+                'Pragma: no-cache'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        preg_match("~<img.*src\s*=\s*[\"']([^\"']+)[\"'][^>]*>~i", $response, $m);
+        return $m[1];
     }
 }
